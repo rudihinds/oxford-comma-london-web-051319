9c1628e9016d74b2d2794bc76b07fbe69d9f53cd
rudi hinds <rudihinds@gmail.com>
2019-05-02 12:34:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -15,8 +15,8 @@ def oxford_comma(array)
 when 1
   "#{array[0]}"
 when 2
-  array[0..1].join(" and ")
+  array.join(" and ")
 else 
-  array[0...-1].join(", ") << ", and #{array[-1]}"
+  array.join(", ") << ", and #{array[-1]}"
 end 
 end  
