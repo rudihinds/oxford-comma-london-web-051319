7780a9da4c3c9a9469460b42ba0d787a26f1338a
rudi hinds <rudihinds@gmail.com>
2019-05-02 12:39:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
+# array = ["fiddleheads","okra","kohlrabi", "obladi", "oblada"]
+
+# def oxford_comma(array)
+  
+#   array[-1] = "and #{array[-1]}"
+#   array.join(", ")
+ 
+  
+# end
+
+
 def oxford_comma(array)
 
-end
+   case array.length 
+when 1
+  "#{array[0]}"
+when 2
+  array.join(" and ")
+else 
+  array[0...-1].join(", ") << ", and #{array[-1]}"
+end 
+end  
